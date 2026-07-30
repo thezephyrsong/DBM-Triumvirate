03e996d9ae9905668d20d52260f4340fabeccfa7
thezephyrsong <116720216+thezephyrsong@users.noreply.github.com>
2026-07-31 02:14:56 +1000

    Bump version to 10.2.1 alpha
    Update core version metadata: set Revision, DisplayVersion and ReleaseRevision in DBM-Core.lua to reflect 10.2.1 alpha. Update DBM-Core.toc: add 'bonkywonky' to Author, set Version to 10.2.1_alpha, and update X-Website to thezephyrsong/DBM-CC.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -82,9 +82,9 @@ local function currentFullDate()
 end
 
 DBM = {
-	Revision = parseCurseDate("20260416000000"),
-	DisplayVersion = "10.1.13 alpha", -- the string that is shown as version
-	ReleaseRevision = releaseDate(2026, 04, 16) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	Revision = parseCurseDate("2026072890711"),
+	DisplayVersion = "10.2.1 alpha", -- the string that is shown as version
+	ReleaseRevision = releaseDate(2026, 07, 28) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
 
 local fakeBWVersion = 7558
