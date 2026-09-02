2c1b1d64b8d5a0382939182da1df6d5c856b6ff3
ZephZae <116720216+thezephyrsong@users.noreply.github.com>
2026-09-02 19:17:48 +1000

    Update version to 10.2.2 alpha and release date

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -82,9 +82,9 @@ local function currentFullDate()
 end
 
 DBM = {
-	Revision = parseCurseDate("2026080400000"),
-	DisplayVersion = "10.2.1 alpha", -- the string that is shown as version
-	ReleaseRevision = releaseDate(2026, 08, 04) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	Revision = parseCurseDate("2026090100000"),
+	DisplayVersion = "10.2.2 alpha", -- the string that is shown as version
+	ReleaseRevision = releaseDate(2026, 09, 01) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
 
 local fakeBWVersion = 7558
