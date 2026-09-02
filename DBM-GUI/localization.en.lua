aac34a4b506e94e5195996e6b7d610b85c4db07f
ZephZae <116720216+thezephyrsong@users.noreply.github.com>
2026-09-02 19:22:57 +1000

    Update localization for Triumvirate project

--- DBM-GUI/localization.en.lua
+++ DBM-GUI/localization.en.lua
@@ -5,10 +5,10 @@ local CL = DBM_CORE_L
 
 L.MainFrame 						= CL.DEADLY_BOSS_MODS -- NO TRANSLATE
 
-L.DBMWarmane						= CL.DBM .. "-Chromiecraft"
+L.DBMWarmane						= CL.DBM .. "-Triumvirate"
 L.TranslationByPrefix				= "Retail backport by "
-L.TranslationBy						= "Barsoom, Bunny67, Zidras"
-L.Website							= "Updates on GitHub at |cFF73C2FBhttps://github.com/5Buttons/DBM-Chromiecraft|r"
+L.TranslationBy						= "Yuna, bonkywonky, Barsoom, Bunny67, Zidras"
+L.Website							= "Updates on GitHub at |cFF73C2FBhttps://github.com/thezephyrsong/DBM-Triumvirate|r"
 L.WebsiteButton						= "Website"
 
 L.OTabBosses						= "Boss Options"--Deprecated and will be deleted once tabs no longer use this
